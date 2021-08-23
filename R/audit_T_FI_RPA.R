dcdaf086dc18925f9e4ff4dcdf8cca4ba558f2df
takewiki <hulilei@takewiki.com.cn>
2021-08-23 19:16:11 +0800

    v2.1

--- R/audit_T_FI_RPA.R
+++ R/audit_T_FI_RPA.R
@@ -61,3 +61,73 @@ order by FBrand,FChannel,FRptItemNumber")
 
 
 
+
+#' 审计报表进行备注查询
+#'
+#' @param conn 连接
+#' @param FYear 年份
+#' @param FPeriod 期间
+#' @param FBrand 品牌
+#' @param FChannel 渠道
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' audit_fi_rpa_getValue()
+audit_fi_rpa_getValue <- function(conn=tsda::conn_rds('jlrds'),
+                             FYear = 2021,
+                             FPeriod =6,
+                             FBrand = 'COMO',
+                             FChannel = '电商' ,
+                             FRptItemNumber='I01'
+) {
+
+  sql <- paste0("select FRptAmt,FRemark  from t_mrpt_target
+where Fyear = ",FYear,"  and Fperiod = ",FPeriod," and
+Fbrand = '",FBrand,"' and Fchannel = '",FChannel,"'
+and FRptItemNumber = '",FRptItemNumber,"'")
+  res <- tsda::sql_select(conn,sql)
+  FRemark <- tsdo::na_replace(res$FRemark,'')
+  FRptAmt <- as.numeric(res$FRptAmt)
+  info <- list(FRptAmt,FRemark)
+  names(info) <- c('FRptAmt','FRemark')
+  return(info)
+
+}
+
+#' 审计报表进行备注查询
+#'
+#' @param conn 连接
+#' @param FYear 年份
+#' @param FPeriod 期间
+#' @param FBrand 品牌
+#' @param FChannel 渠道
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' audit_fi_rpa_getValue()
+audit_fi_rpa_setValue <- function(conn=tsda::conn_rds('jlrds'),
+                                  FYear = 2021,
+                                  FPeriod =6,
+                                  FBrand = 'COMO',
+                                  FChannel = '电商' ,
+                                  FRptItemNumber='I01',
+                                  FRptAmt = 1.08,
+                                  FRemark ='S009方案的BW报表缺少F30客户维度，RPA重新取数'
+
+) {
+
+  sql <- paste0("update a set a.FRptAmt = ",FRptAmt,",a.FRemark = '",FRemark,"'  from t_mrpt_target a
+where Fyear = ",FYear,"  and Fperiod = ",FPeriod," and
+Fbrand = '",FBrand,"' and Fchannel = '",FChannel,"'
+and FRptItemNumber = '",FRptItemNumber,"'")
+   tsda::sql_update(conn,sql)
+
+
+}
+
+
+
